f17a0cd7e9183cfab85dee74ec3e7148ac394f0d
timotei <timotei_cluj@yahoo.co.uk>
2011-12-04 11:09:45 +0200

    fix some double to float conversion warnings

--- src/Components/Skybox.cpp
+++ src/Components/Skybox.cpp
@@ -29,55 +29,55 @@ void Skybox::draw()
 
 	glBindTexture(GL_TEXTURE_2D, _textures[LEFT]);
 	glBegin(GL_QUADS);
-		glTexCoord2f(1, 1);glVertex3f(-_width+0.1, _height, -_thickness);
-		glTexCoord2f(0, 1);glVertex3f(-_width+0.1, _height, _thickness);
-		glTexCoord2f(0, 0);glVertex3f(-_width+0.1, -_height, _thickness);
-		glTexCoord2f(1, 0);glVertex3f(-_width+0.1, -_height, -_thickness);
+		glTexCoord2f(1.0f, 1.0f);glVertex3f(-_width+0.1f, _height, -_thickness);
+		glTexCoord2f(0.0f, 1.0f);glVertex3f(-_width+0.1f, _height, _thickness);
+		glTexCoord2f(0.0f, 0.0f);glVertex3f(-_width+0.1f, -_height, _thickness);
+		glTexCoord2f(1.0f, 0.0f);glVertex3f(-_width+0.1f, -_height, -_thickness);
 	glEnd();
 
 	//negative y plane
 	glBindTexture(GL_TEXTURE_2D, _textures[DOWN]);
 	glBegin(GL_QUADS);
-		glTexCoord2f(1, 1);glVertex3f(_width, -_height, -_thickness);
-		glTexCoord2f(0, 1);glVertex3f(-_width, -_height, -_thickness);
-		glTexCoord2f(0, 0);glVertex3f(-_width, -_height, _thickness);
-		glTexCoord2f(1, 0);glVertex3f(_width, -_height, _thickness);
+		glTexCoord2f(1.0f, 1.0f);glVertex3f(_width, -_height, -_thickness);
+		glTexCoord2f(0.0f, 1.0f);glVertex3f(-_width, -_height, -_thickness);
+		glTexCoord2f(0.0f, 0.0f);glVertex3f(-_width, -_height, _thickness);
+		glTexCoord2f(1.0f, 0.0f);glVertex3f(_width, -_height, _thickness);
 		glEnd();
 
 	//negative z plane
 	glBindTexture(GL_TEXTURE_2D, _textures[BACK]);
 	glBegin(GL_QUADS);
-		glTexCoord2f(1, 1);glVertex3f(-_width, _height, _thickness -0.1);
-		glTexCoord2f(0, 1);glVertex3f(_width, _height, _thickness -0.1);
-		glTexCoord2f(0, 0);glVertex3f(_width, -_height, _thickness -0.1);
-		glTexCoord2f(1, 0);glVertex3f(-_width, -_height, _thickness -0.1);
+		glTexCoord2f(1.0f, 1.0f);glVertex3f(-_width, _height, _thickness -0.1f);
+		glTexCoord2f(0.0f, 1.0f);glVertex3f(_width, _height, _thickness -0.1f);
+		glTexCoord2f(0.0f, 0.0f);glVertex3f(_width, -_height, _thickness -0.1f);
+		glTexCoord2f(1.0f, 0.0f);glVertex3f(-_width, -_height, _thickness -0.1f);
 	glEnd();
 
 	//positive x plane
 	glBindTexture(GL_TEXTURE_2D, _textures[RIGHT]);
 	glBegin(GL_QUADS);
-		glTexCoord2f(1, 1);glVertex3f(_width-0.1, _height, _thickness);
-		glTexCoord2f(0, 1);glVertex3f(_width-0.1, _height, -_thickness);
-		glTexCoord2f(0, 0);glVertex3f(_width-0.1, -_height, -_thickness);
-		glTexCoord2f(1, 0);glVertex3f(_width-0.1, -_height, _thickness);
+		glTexCoord2f(1.0f, 1.0f);glVertex3f(_width-0.1f, _height, _thickness);
+		glTexCoord2f(0.0f, 1.0f);glVertex3f(_width-0.1f, _height, -_thickness);
+		glTexCoord2f(0.0f, 0.0f);glVertex3f(_width-0.1f, -_height, -_thickness);
+		glTexCoord2f(1.0f, 0.0f);glVertex3f(_width-0.1f, -_height, _thickness);
 	glEnd();
 
 	//positive y plane
 	glBindTexture(GL_TEXTURE_2D, _textures[UP]);
 	glBegin(GL_QUADS);
-		glTexCoord2f(1, 1);glVertex3f(_width, _height, _thickness);
-		glTexCoord2f(0, 1);glVertex3f(-_width, _height, _thickness);
-		glTexCoord2f(0, 0);glVertex3f(-_width, _height, -_thickness);
-		glTexCoord2f(1, 0);glVertex3f(_width, _height, -_thickness);
+		glTexCoord2f(1.0f, 1.0f);glVertex3f(_width, _height, _thickness);
+		glTexCoord2f(0.0f, 1.0f);glVertex3f(-_width, _height, _thickness);
+		glTexCoord2f(0.0f, 0.0f);glVertex3f(-_width, _height, -_thickness);
+		glTexCoord2f(1.0f, 0.0f);glVertex3f(_width, _height, -_thickness);
 	glEnd();
 
 	//positive z plane
 	glBindTexture(GL_TEXTURE_2D, _textures[FRONT]);
 	glBegin(GL_QUADS);
-		glTexCoord2f(1, 1);glVertex3f(_width, _height, -_thickness+0.1);
-		glTexCoord2f(0, 1);glVertex3f(-_width, _height, -_thickness+0.1);
-		glTexCoord2f(0, 0);glVertex3f(-_width, -_height, -_thickness+0.1);
-		glTexCoord2f(1, 0);glVertex3f(_width, -_height, -_thickness+0.1);
+		glTexCoord2f(1.0f, 1.0f);glVertex3f(_width, _height, -_thickness+0.1f);
+		glTexCoord2f(0.0f, 1.0f);glVertex3f(-_width, _height, -_thickness+0.1f);
+		glTexCoord2f(0.0f, 0.0f);glVertex3f(-_width, -_height, -_thickness+0.1f);
+		glTexCoord2f(1.0f, 0.0f);glVertex3f(_width, -_height, -_thickness+0.1f);
 	glEnd();
 
 	glDisable(GL_TEXTURE_2D);
