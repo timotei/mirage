d14413f3fd3e3945edcf1cb29e64dcc1faeef402
timotei <timotei_cluj@yahoo.co.uk>
2011-12-06 00:57:31 +0200

    fix the light rotation

--- src/Components/LightSource.cpp
+++ src/Components/LightSource.cpp
@@ -12,6 +12,12 @@ LightSource::~LightSource()
 
 void LightSource::draw()
 {
+	glPushMatrix();
+
+	glRotatef(rotation.x, 1, 0, 0);
+	glRotatef(rotation.y, 0, 1, 0);
+	glRotatef(rotation.z, 0, 0, 1);
+
 	glLightfv(_lightNum, GL_POSITION, position.toArray().get());
 
 	glLightfv(_lightNum, GL_DIFFUSE, diffuse.toArray().get());
@@ -23,13 +29,9 @@ void LightSource::draw()
 	glLightfv(_lightNum, GL_SHININESS, shine);
 	
 	glDisable(GL_LIGHTING);
-	glPushMatrix();
-		glRotatef(rotation.x, 1, 0, 0);
-		glRotatef(rotation.y, 0, 1, 0);
-		glRotatef(rotation.z, 0, 0, 1);
-
-		glTranslatef(modelPosition.x, modelPosition.y, modelPosition.z);
-		glutSolidSphere(1, 10, 10);
-	glPopMatrix();
+	glTranslatef(modelPosition.x, modelPosition.y, modelPosition.z);
+	glutSolidSphere(1, 10, 10);
 	glEnable(GL_LIGHTING);
+
+	glPopMatrix();
 }
