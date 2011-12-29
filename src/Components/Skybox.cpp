bc74895e55bfb2d714ada954a0b19775d382ece2
timotei <timotei_cluj@yahoo.co.uk>
2011-12-29 20:14:35 +0200

    add the propers normals to the skybox

--- src/Components/Skybox.cpp
+++ src/Components/Skybox.cpp
@@ -20,20 +20,22 @@ Skybox::Skybox(float width, float height, float thickness) :
 
 void Skybox::loadTextures(std::string texturePrefix)
 {
-	loadTGA(const_cast<char*>((texturePrefix + "_left.tga").c_str()), _textures[LEFT]);	
-	loadTGA(const_cast<char*>((texturePrefix + "_down.tga").c_str()), _textures[DOWN]);	
-	loadTGA(const_cast<char*>((texturePrefix + "_front.tga").c_str()), _textures[FRONT]);	
-	loadTGA(const_cast<char*>((texturePrefix + "_right.tga").c_str()), _textures[RIGHT]);	
-	loadTGA(const_cast<char*>((texturePrefix + "_up.tga").c_str()), _textures[UP]);	
-	loadTGA(const_cast<char*>((texturePrefix + "_back.tga").c_str()), _textures[BACK]);	
+	loadTGA((texturePrefix + "_left.tga").c_str(), _textures[LEFT]);	
+	loadTGA((texturePrefix + "_down.tga").c_str(), _textures[DOWN]);	
+	loadTGA((texturePrefix + "_front.tga").c_str(), _textures[FRONT]);	
+	loadTGA((texturePrefix + "_right.tga").c_str(), _textures[RIGHT]);	
+	loadTGA((texturePrefix + "_up.tga").c_str(), _textures[UP]);	
+	loadTGA((texturePrefix + "_back.tga").c_str(), _textures[BACK]);	
 }
 
 void Skybox::draw()
 {
+	glPushMatrix();
 	glEnable(GL_TEXTURE_2D);
 
 	glBindTexture(GL_TEXTURE_2D, _textures[LEFT]);
 	glBegin(GL_QUADS);
+		glNormal3f(1,0,0);
 		glTexCoord2f(1.0f, 1.0f);glVertex3f(-_width+0.1f, _height, -_thickness);
 		glTexCoord2f(0.0f, 1.0f);glVertex3f(-_width+0.1f, _height, _thickness);
 		glTexCoord2f(0.0f, 0.0f);glVertex3f(-_width+0.1f, -_height, _thickness);
@@ -43,6 +45,7 @@ void Skybox::draw()
 	//negative y plane
 	glBindTexture(GL_TEXTURE_2D, _textures[DOWN]);
 	glBegin(GL_QUADS);
+		glNormal3f(0,1,0);
 		glTexCoord2f(1.0f, 1.0f);glVertex3f(_width, -_height, -_thickness);
 		glTexCoord2f(0.0f, 1.0f);glVertex3f(-_width, -_height, -_thickness);
 		glTexCoord2f(0.0f, 0.0f);glVertex3f(-_width, -_height, _thickness);
@@ -52,6 +55,7 @@ void Skybox::draw()
 	//negative z plane
 	glBindTexture(GL_TEXTURE_2D, _textures[BACK]);
 	glBegin(GL_QUADS);
+		glNormal3f(0,0,-1);
 		glTexCoord2f(1.0f, 1.0f);glVertex3f(-_width, _height, _thickness -0.1f);
 		glTexCoord2f(0.0f, 1.0f);glVertex3f(_width, _height, _thickness -0.1f);
 		glTexCoord2f(0.0f, 0.0f);glVertex3f(_width, -_height, _thickness -0.1f);
@@ -61,6 +65,7 @@ void Skybox::draw()
 	//positive x plane
 	glBindTexture(GL_TEXTURE_2D, _textures[RIGHT]);
 	glBegin(GL_QUADS);
+		glNormal3f(-1,0,0);
 		glTexCoord2f(1.0f, 1.0f);glVertex3f(_width-0.1f, _height, _thickness);
 		glTexCoord2f(0.0f, 1.0f);glVertex3f(_width-0.1f, _height, -_thickness);
 		glTexCoord2f(0.0f, 0.0f);glVertex3f(_width-0.1f, -_height, -_thickness);
@@ -70,6 +75,7 @@ void Skybox::draw()
 	//positive y plane
 	glBindTexture(GL_TEXTURE_2D, _textures[UP]);
 	glBegin(GL_QUADS);
+		glNormal3f(0,-1,0);
 		glTexCoord2f(1.0f, 1.0f);glVertex3f(_width, _height, _thickness);
 		glTexCoord2f(0.0f, 1.0f);glVertex3f(-_width, _height, _thickness);
 		glTexCoord2f(0.0f, 0.0f);glVertex3f(-_width, _height, -_thickness);
@@ -79,11 +85,14 @@ void Skybox::draw()
 	//positive z plane
 	glBindTexture(GL_TEXTURE_2D, _textures[FRONT]);
 	glBegin(GL_QUADS);
+		glNormal3f(0,0,1);
 		glTexCoord2f(1.0f, 1.0f);glVertex3f(_width, _height, -_thickness+0.1f);
 		glTexCoord2f(0.0f, 1.0f);glVertex3f(-_width, _height, -_thickness+0.1f);
 		glTexCoord2f(0.0f, 0.0f);glVertex3f(-_width, -_height, -_thickness+0.1f);
 		glTexCoord2f(1.0f, 0.0f);glVertex3f(_width, -_height, -_thickness+0.1f);
 	glEnd();
 
+	glBindTexture(GL_TEXTURE_2D, 0);
 	glDisable(GL_TEXTURE_2D);
+	glPopMatrix();
 }
