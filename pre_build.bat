822543ddee0c2d4b07df662a4f7d8c54e6c0ad94
timotei <timotei_cluj@yahoo.co.uk>
2011-12-29 20:52:22 +0200

    regenerate the lua module only if needed

--- pre_build.bat
+++ pre_build.bat
@@ -1,1 +1,13 @@
+@echo off
+cd src/Lua
+SET LUASOURCE=LuaMirage.cpp
+SET LUAPACKAGE=luamirage.pkg
+FOR /F %%i IN ('DIR /B /O:D "%LUASOURCE%" "%LUAPACKAGE%"') DO SET NEWEST=%%i 
+cd ..
+cd ..
+
+IF %NEWEST% EQU %LUAPACKAGE% (
+echo Regenerating lua module from package...
 "external/tolua++.exe" -o src/Lua/LuaMirage.cpp src/Lua/luamirage.pkg
+echo done
+)
