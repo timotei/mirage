204ef6e4ac596e55152eb0ac3aff83a462c2db31
timotei <timotei_cluj@yahoo.co.uk>
2011-12-30 12:35:21 +0200

    Move the project files into their own directory

--- projectfiles/VC9/pre_build.bat
+++ projectfiles/VC9/pre_build.bat
@@ -1,5 +1,5 @@
 @echo off
-cd src/Lua
+cd ../../src/Lua
 SET LUASOURCE=LuaMirage.cpp
 SET LUAPACKAGE=luamirage.pkg
 FOR /F %%i IN ('DIR /B /O:D "%LUASOURCE%" "%LUAPACKAGE%"') DO SET NEWEST=%%i 
@@ -10,4 +10,6 @@ IF %NEWEST% EQU %LUAPACKAGE% (
 echo Regenerating lua module from package...
 "external/tolua++.exe" -o src/Lua/LuaMirage.cpp src/Lua/luamirage.pkg
 echo done
+) ELSE (
+echo Skipping regeneration. No action needed.
 )
