333d00803e73c001a154e287ad1b8b44637c27ba
timotei <timotei_cluj@yahoo.co.uk>
2012-01-09 13:31:37 +0200

    Add the license

--- src/Components/LightSource.cpp
+++ src/Components/LightSource.cpp
@@ -1,3 +1,17 @@
+/*
+   Copyright (C) 2011-2012 by Timotei Dolean <timotei21@gmail.com>
+
+   Part of the Mirage Project https://github.com/timotei/mirage
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
 #include "LightSource.hpp"
 
 #include <sstream>
